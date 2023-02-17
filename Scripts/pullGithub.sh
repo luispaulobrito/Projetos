90269da4d7cbc03877cddb2d62e3d87b0bfe936e
Luis Paulo Brito <luiss_paullo@hotmail.com>
2023-02-17 14:52:46 -0300

    Scripts

---
+++ Scripts/pullGithub.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull
+echo "Resposta do Github:"
+git status
---
+++ Scripts/pushGithub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git add .
+echo "Adicionado"
+git status
+
+read -p "Digite a mensagem" mensagem
+git commit -m "$mensagem"
+echo "Comitado"
+git status
+
+git push
+echo "Resultado:"
+git status
