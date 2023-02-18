cec809e908aa69bd72be29d72c21cb258ea78a2a
Luis Paulo Brito <luiss_paullo@hotmail.com>
2023-02-17 21:53:15 -0300

    script pushgithub melhorado

--- pushGithub.sh
+++ pushGithub.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+git pull
+git status
+
 git add .
-echo "Adicionado"
 git status
 
 read -p "Digite a mensagem: " mensagem
 git commit -m "$mensagem"
-echo "Comitado"
 git status
 
 git push
-echo "Resultado:"
 git status
