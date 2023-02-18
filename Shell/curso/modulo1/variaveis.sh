534ff279ff12c0ac3076b78aa225d0299a97d486
Luis Paulo Brito <luiss_paullo@hotmail.com>
2023-02-17 21:49:19 -0300

    pasta curso shell script adicionada

---
+++ Shell/curso/modulo1/variaveis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ Pessoa1="Luis"
+ Pessoa2="José"
+
+ idadePessoa1=33
+ idadePessoa2=45
+
+total=$((idadePessoa1 + idadePessoa2))
+
+ echo "$Pessoa1 tem  $idadePessoa1 e $Pessoa2 tem $idadePessoa2, somadas as idades dá: $total"
